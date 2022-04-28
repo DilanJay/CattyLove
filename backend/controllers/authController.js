9af05d21b432d71d51a9e112d2d4bde317b2059e
pubudi <pubudiherath@gmai.com>
2022-04-28 21:39:49 +0530

    Change password API endpoint

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -54,39 +54,74 @@ const register = async (req, res, next) => {
       next();
     }
   };
-
+  
   const login = async (req, res) => {
-      try{
-        const { username, password } = req.body
-        const user = await User.findOne({ username }).lean()
-    
-        if (!user) {
-            return res.json({ status: 'error', error: 'Invalid username/password' })
-        }
-    
-        if (await bcrypt.compare(password, user.password)) {
-            // the username, password combination is successful
-    
-            const token = jwt.sign(
-                {
-                    id: user._id,
-                    username: user.username
-                },
-                JWT_SECRET
-            )
-    
-            return res.json({ status: 'ok', data: token })
-        }
-    
-        res.json({ status: 'error', error: 'Invalid username/password' })
-      }catch (error) {
+      try{ 
+      const { username, password } = req.body
+      const user = await User.findOne({ username }).lean()
+  
+      if (!user) {
+          return res.json({ status: 'error', error: 'Invalid username/password' })
+      }
+  
+      if (await bcrypt.compare(password, user.password)) {
+          // the username, password combination is successful
+  
+          const token = jwt.sign(
+              {
+                  id: user._id,
+                  username: user.username
+              },
+              JWT_SECRET
+          )
+  
+          return res.json({ status: 'ok', data: token })
+      }
+  
+      res.json({ status: 'error', error: 'Invalid username/password' })
+    }catch (error) {
         console.error(error);
         res.status(500).json({ message: "Server Error", err: error });
         next();
       }
-	
-}
-
-module.exports = {
-    register
-}
+  }
+  
+  const changepassword = async (req, res) => {
+      const { token, newpassword: plainTextPassword } = req.body
+  
+      if (!plainTextPassword || typeof plainTextPassword !== 'string') {
+          return res.json({ status: 'error', error: 'Invalid password' })
+      }
+  
+      if (plainTextPassword.length < 5) {
+          return res.json({
+              status: 'error',
+              error: 'Password too small. Should be atleast 6 characters'
+          })
+      }
+  
+      try {
+          const user = jwt.verify(token, JWT_SECRET)
+  
+          const _id = user.id
+  
+          const password = await bcrypt.hash(plainTextPassword, 10)
+  
+          await User.updateOne(
+              { _id },
+              {
+                  $set: { password }
+              }
+          )
+          res.json({ status: 'ok' })
+      } catch (error) {
+          console.log(error)
+          res.json({ status: 'error', error: ';))' })
+      }
+  }
+  
+  module.exports = {
+    register,
+    login,
+    changepassword,
+  };
--- backend/routes/authRoute.js
+++ backend/routes/authRoute.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const {register} = require('../controllers/authController'); 
+const {register,login} = require('../controllers/authController'); 
 // register user
 router.route('/register').post(register);
 router.route('/login').post(login);
