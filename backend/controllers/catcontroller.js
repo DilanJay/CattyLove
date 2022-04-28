52489aae58218285d51eeb624b196a95398165af
Heshani chamalka <63486300+Heshani-96@users.noreply.github.com>
2022-04-28 16:46:17 +0530

    Create catcontroller.js

---
+++ backend/controllers/catcontroller.js
@@ -0,0 +1,26 @@
+const Cat = require("../models/cat");
+
+const addCat = async (req, res, next) => {
+    try {
+      let requestBody = req.body;
+  
+      if (requestBody != null) {
+        const cat = await Cat.create({ ...requestBody });
+        console.log(req);
+        res.status(200).json(requestBody);
+      } else {
+        res.status(406).json({
+          data: requestBody,
+          message: "please fill all requires fields",
+        });
+      }
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: "Server Error", err: error });
+      next();
+    }
+  };
+
+  module.exports = {
+    addCat,
+  };
