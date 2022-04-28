c566d839179cfa898aa3f2582772814afef06d5a
pubudi <pubudiherath@gmai.com>
2022-04-28 21:45:21 +0530

    Add erroHandle middleware

--- backend/routes/authRoute.js
+++ backend/routes/authRoute.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-const {register,login} = require('../controllers/authController'); 
+const {register,login,changepassword} = require('../controllers/authController'); 
 // register user
 router.route('/register').post(register);
 router.route('/login').post(login);
+router.route('/changepassword').post(changepassword);
 module.exports = router;
