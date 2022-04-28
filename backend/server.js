9c11b46a50ec4a7a13f347b908a4a885f27bca94
Ruchira Wishwajith <dilan.pramodya97@gmail.com>
2022-04-28 16:20:58 +0530

    Create Server.js &  config database

---
+++ backend/server.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const connectDB = require("./config/db");
+
+var cors = require("cors");
+connectDB();
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// create new cat
+// get all cat data
+// get single cat data
+// update like count
+// delete cat
+
+
+// add comment
+
+// create new user
+// read user
+// update user
+// delete user
+
+
+// create wishlist
+// update wishlist
+// get all from wishlist
+// get one from wishlist
+// delete wishlist
+
+ 
+
+const PORT = process.env.PORT || 5201; // set the port
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`)); // log to console.
+
+// if there are any unhandledRejection, then this function will log it and close the serever.
+process.on("unhand  ledRejection", (err, promise) => {
+  console.log(`Logged Error: ${err.message}`);
+  server.close(() => process.exit(1));
+});
