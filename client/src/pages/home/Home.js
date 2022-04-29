b3e63f11b55e4e301d511cfe365ee825a8b69554
pubudi <pubudiherath@gmai.com>
2022-04-29 23:00:49 +0530

    home page minor issue fix

--- client/src/pages/home/Home.js
+++ client/src/pages/home/Home.js
@@ -1,8 +1,9 @@
-import React from "react";
+import React , { useEffect } from "react";
 import StyleHeding from "../../component/honeheader/StyleHeding";
 import Imagetable from "../../component/imagetable/Imagetable";
 
 function Home() {
+  useEffect(() => {}, []);
   return (
     <div className="container emp-profile">
       <StyleHeding name="Cat List"></StyleHeding>
