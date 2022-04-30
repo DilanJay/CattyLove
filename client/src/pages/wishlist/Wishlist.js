0094cda353f4bb343919802a74e2436a8710ed4d
Heshani chamalka <63486300+Heshani-96@users.noreply.github.com>
2022-04-30 13:31:41 +0530

    Update Wishlist.js

--- client/src/pages/wishlist/Wishlist.js
+++ client/src/pages/wishlist/Wishlist.js
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import StyleHeding from "../../component/honeheader/StyleHeding";
 import Imagetable from "../../component/imagetable/Imagetable";
 
 function Wishlist() {
+  const [isUserLoggedIn, setIsUserLoggedIn] = useState(false);
+  useEffect(() => {
+    if (localStorage.getItem("authToken")) {
+      setIsUserLoggedIn(true);
+    } else {
+      setIsUserLoggedIn(false);
+    }
+  }, []);
   return (
     <div className="container emp-profile">
       <StyleHeding name="Your Wishlist"></StyleHeding>
-      <Imagetable loadtype="All" />
+      {isUserLoggedIn ? (
+        <Imagetable loadtype="whislist" />
+      ) : (
+        <div className="text-center">
+          <h3>You are not logged in please login to view your wishlist</h3>
+        </div>
+      )}
     </div>
   );
 }
